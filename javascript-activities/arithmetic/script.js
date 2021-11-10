76ba270c096922ce7d7944c3c6b49387ea388f30
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-11-10 12:30:40 -0800

    created arithmetic activity

---
+++ javascript-activities/arithmetic/script.js
@@ -0,0 +1,16 @@
+// Fill in the body of each function with the code required
+// One at a time, uncomment the line that starts with: var 
+// Don't forget to name your variable
+
+
+// THIS SHOULD ADD NUM1 FROM NUM2 
+// var  = function(num1, num2) {};
+
+// THIS SHOULD SUBTRACT NUM1 FROM NUM2 
+// var  = function(num1, num2) {};
+
+// THIS SHOULD MULTIPLY NUM1 & NUM2 
+// var multiply = function(num1, num2) {};
+
+// THIS SHOULD DIVIDE NUM1 FROM NUM2 
+// var divide = function(num1, num2) {};
