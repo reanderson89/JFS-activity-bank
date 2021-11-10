49f2cfca74de544951d7903639521528ab99107c
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2021-11-10 14:52:54 -0800

    quick change

--- javascript-activities/arithmetic/script.js
+++ javascript-activities/arithmetic/script.js
@@ -1,18 +1,17 @@
-// Fill in the body of each function with the code required
 // One at a time, uncomment the line that starts with: let 
 // Don't forget to name your variable
 
 
 // THIS SHOULD ADD
-// let  = 1 + 1;
+// let  = ;
 
 // THIS SHOULD SUBTRACT NUM1 FROM NUM2 
-// let  = 3 - 1;
+// let  = ;
 
 // THIS SHOULD MULTIPLY NUM1 & NUM2 
-// let  = 1 * 3;
+
 
 // THIS SHOULD DIVIDE NUM1 FROM NUM2 
-// let  = 8 / 2;
+
 
 
