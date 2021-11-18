229f6d5a5f90ee290e9fc52aafa0a94bedc032ba
robAnderson <reanderson89@gmail.com>
2021-11-18 10:28:34 -0800

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// loop 1
+// let i = 1;
+
+// const loopyFunc = () => {
+//     if(i <= 50){
+//         console.log(i);
+//         i++;
+//         loopyFunc();
+//     }
+// }
+
+// loopyFunc();
+
