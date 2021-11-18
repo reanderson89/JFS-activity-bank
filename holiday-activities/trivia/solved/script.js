5f5bc7c3ad55c785cc5361a5890d06798211df41
robAnderson <reanderson89@gmail.com>
2021-11-18 14:18:27 -0800

    updated

--- holiday-activities/trivia/solved/script.js
+++ holiday-activities/trivia/solved/script.js
@@ -3,10 +3,9 @@ alert("Please input either 'a' , 'b' , 'c' , or 'd' only for each question.")
 let question1 = "Who won the 2021 World Series? \n a. Atlanta Braves \n b. Houston Astros \n c. New York Yankees \n d. Athletics"
 let question2 = "What is the capital of Oregon? \n a. Seattle \n b. Salem \n c. Miami \n d. Los Angeles"
 let question3 = "What is 3x+2, when x = -2? \n a. 8 \n b. 4 \n c. -4 \n d. 0"
-let question4 = "What color is made when you combine yellow and red? \n a. Green \n b. Grey \n c. Blue \n d. Orange"
 
-const questionsArray = [question1,question2,question3,question4]
-const correctAnswers = ["a","b","c","d"]
+const questionsArray = [question1,question2,question3]
+const correctAnswers = ["a","b","c"]
 
 let correctCounter = 0;
 
