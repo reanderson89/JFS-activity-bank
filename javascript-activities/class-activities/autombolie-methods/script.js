547a1de5220bada0aaf0964ee5e78a7adc1527c2
robAnderson <reanderson89@gmail.com>
2021-11-29 21:29:19 -0800

    added automobile methods activity

---
+++ javascript-activities/class-activities/autombolie-methods/script.js
@@ -0,0 +1,16 @@
+class AutoMobile {
+    constructor(type, numOfWheels, color){
+        this.type = type;
+        this.numOfWheels = numOfWheels;
+        this.color = color;
+        this.isWorking = true;
+    }
+
+    // add a getter for each property that returns its value
+
+
+    // add a method that changes isWorking to false and also prints to the console that it needs a tune up
+
+
+}
+
