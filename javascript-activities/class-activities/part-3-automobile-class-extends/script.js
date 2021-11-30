f522184fc1afd7966cb1498b426333bb367b7134
robAnderson <reanderson89@gmail.com>
2021-11-29 22:40:31 -0800

    update

--- javascript-activities/class-activities/part-3-automobile-class-extends/script.js
+++ javascript-activities/class-activities/part-3-automobile-class-extends/script.js
@@ -31,7 +31,5 @@ class AutoMobile {
 // create a child class that extends the parent class, for example a "Truck" class
 // This class should have 5 parameters, the first 3 being the same as the parent class: type, numOfWheels, color.
 // The second two are up to you. They could be: numOfDoors, towingCapacity.
-// Also, create a method that is unique to the child class. For example a method called canItTowIt(trailer){} that checks if the towing capacity is greater then the number passed into the method.
+// Also, create a method that is unique to the child class. For example a method called canItTowIt(trailerWeight){} that checks if the towing capacity is greater then the number passed into the method.
 // after you create the child class, create an instance of the class and console.log it and run the method you created.
-
-
