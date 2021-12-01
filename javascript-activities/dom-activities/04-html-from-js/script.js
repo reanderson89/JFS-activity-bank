021ee286b49448d5bc3bcb75fa74df54039fcc11
robAnderson <reanderson89@gmail.com>
2021-12-01 14:19:42 -0800

    added new activities

---
+++ javascript-activities/dom-activities/04-html-from-js/script.js
@@ -0,0 +1,13 @@
+// DOM variables go here
+
+
+
+// Perform your DOM manipulation on the <p> element here
+
+
+
+// check out this webpage for help on the image:
+//  https://www.w3schools.com/jsref/met_element_setattribute.asp
+
+
+
---
+++ javascript-activities/dom-activities/05-loop-list-from-js/script.js
@@ -0,0 +1,33 @@
+// For this activity you will have to create an <ul> element using DOM manipulation and then add items from the given array as <li> to that list.
+// Useful links
+// How to create an element: https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+// How to add text to an element: https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+// How to append an element to another: https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+
+
+
+
+// DOM variables go here
+let listHolder = document.getElementById("ulGoesHere");
+
+
+
+// array to loop through for list items
+let groceryList = ["bread", "eggs", "squash", "chicken", "almonds", "pasta", "beans"];
+
+//  Step-1. create a variable that holds a new <ul> element
+
+// Step-2. create a for loop that will loop through the entire array, you could also use the .forEach() method
+
+    // Steps 3-5 all take place inside of the loop
+    // Step-3. create a variable and give it the value of a new "li" element
+
+    // Step-4. set the innerHTML of that element to the current item of the groceryList
+
+    // Step-5. add each new item to the "ul" you created before the loop
+
+
+
+// Step-6. append the the list with all of the new list items to the listHolder variable so it will display on the page
+
+
