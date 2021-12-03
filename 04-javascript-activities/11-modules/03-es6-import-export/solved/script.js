46ce12e4294df0ef8e972de77e5db92c5e009238
robAnderson <reanderson89@gmail.com>
2021-12-03 14:02:25 -0800

    added solved folders for activities

---
+++ 04-javascript-activities/11-modules/03-es6-import-export/solved/script.js
@@ -0,0 +1,10 @@
+// documentation:
+//  https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
+
+// create your import here
+import {capitalizeEveryOtherCharacter} from "./utils.js";
+
+// call the function here and pass in a string, then open the browser and check the console
+capitalizeEveryOtherCharacter("hello world");
---
+++ 04-javascript-activities/11-modules/03-es6-import-export/solved/utils.js
@@ -0,0 +1,28 @@
+// You do not need to do anything to the function
+
+const capitalizeEveryOtherCharacter = (string) => {
+let lowerCaseString = string.toLowerCase();
+let stringArray = lowerCaseString.split("");
+let newStringArray = [];
+
+for(let i = 0; i < stringArray.length; i++){
+    if(i % 2 === 0){
+        let upperChar = stringArray[i].toUpperCase();
+        newStringArray.push(upperChar);
+    } else {
+        newStringArray.push(stringArray[i]);
+    }
+}
+let newStr = newStringArray.join("");
+console.log(newStr);
+}
+
+// create your own function that does whatever you want and add it to your export object then use it in your script.js
+
+
+
+// create your export down here
+export{capitalizeEveryOtherCharacter}
+
+
+
